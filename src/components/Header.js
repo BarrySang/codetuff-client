27ef59d6af81d1824641da4c2b7a62d70efaeb20
BarrySang <barrysang8@gmail.com>
2021-03-15 11:27:52 +0300

    create classes: Header, Body, Footer

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="">
+                <h1 className="text-center">CodeStuff</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
