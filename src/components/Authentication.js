4dcc2f8f15d9024531cff5066369a529443f75bf
BarrySang <barrysang8@gmail.com>
2021-06-11 15:49:34 +0300

    add styling for auth login and signup buttons

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -5,7 +5,7 @@ class Authentication extends React.Component {
     render() {
         return (
             <div className="d-flex justify-content-end auth-container">
-                <a href="#" className="mr-1">Login</a>
+                <a href="#">Login</a>
                 <a href="#">Signup</a>
             </div>
         )
